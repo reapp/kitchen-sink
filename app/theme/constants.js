f94695ee0c0de0211fc89b3434177a1e0a7be19b
natew <natewienert@gmail.com>
2015-01-07 15:34:37 -0800

    Remove unnecessary dots styling

--- app/theme/constants.js
+++ app/theme/constants.js
@@ -2,9 +2,6 @@ module.exports = {
   bgColor: '#eee',
   activeColor: '#fff',
 
-  // Dots
-  dotColor: '0,0,0',
-
   // TitleBar
   titleBarBG: '#3552ff',
   titleBarColor: '#fff'
--- app/theme/styles.js
+++ app/theme/styles.js
@@ -2,6 +2,5 @@ var { makeStyles } = require('reapp-ui');
 var requirer = (name) => require('./styles/' + name);
 
 module.exports = makeStyles(requirer, [
-  'Dots',
   'TitleBar'
 ]);
