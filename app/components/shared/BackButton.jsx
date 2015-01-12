fb037200e17f50525c6320dda56667a206347a24
natew <natewienert@gmail.com>
2015-01-11 19:43:43 -0800

    Add todo for proper text animations on titlebar

--- app/components/shared/BackButton.jsx
+++ app/components/shared/BackButton.jsx
@@ -5,6 +5,10 @@ export default (
   <BackButton onClick={function() {
     // todo: get this to work with the staticcontainers
     // this.props.viewListScrollToStep(0);
+
+    // todo: Change Back to Kitchen Sink, get UI to properly align them
     window.history.back();
-  }} />
+  }}>
+    Back
+  </BackButton>
 );
