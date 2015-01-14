61fabfcc2f0e76ae7eabf00801b5a80616cbd7ec
natew <natewienert@gmail.com>
2015-01-14 08:53:45 -0800

    Moving to shorter syntax for app.js

--- app/app.js
+++ app/app.js
@@ -1,15 +1,8 @@
-import Reapp from 'reapp-platform';
-import Run from 'reapp-routes/react-router/run';
+import Router from 'reapp-routes/react-router';
 import Routes from './routes';
 
 // import our theme
 require('./theme/theme');
 
-// setup some stuff
-Reapp.initTouch();
-
 // run the app
-if (Reapp.Env.CLIENT)
-  Run.renderAsync(Routes);
-else
-  Run.renderSync(Routes);
+Router.run(Routes);
--- app/routes.js
+++ app/routes.js
@@ -1,21 +1,20 @@
-import { route, routes } from 'reapp-routes/react-router/generator';
-
-module.exports = routes(require,
-  route('app', '/', { dir: '' },
-    route('kitchen', '/',
-      route('modals'),
-      route('popovers'),
-      route('tabs'),
-      route('buttons'),
-      route('cards'),
-      route('panels'),
-      route('lists'),
-      route('view-lists'),
-      route('dotted-view-lists'),
-      route('view-frosted'),
-      route('grids'),
-      route('forms')
-    ),
-    route('viewer')
-  )
-);
+module.exports = ({ routes, route }) =>
+  routes(require,
+    route('app', '/', { dir: '' },
+      route('kitchen', '/',
+        route('modals'),
+        route('popovers'),
+        route('tabs'),
+        route('buttons'),
+        route('cards'),
+        route('panels'),
+        route('lists'),
+        route('view-lists'),
+        route('dotted-view-lists'),
+        route('view-frosted'),
+        route('grids'),
+        route('forms')
+      ),
+      route('viewer')
+    )
+  );
