66749a176a4efc552acb7407053e57a1955bc132
natew <natewienert@gmail.com>
2015-01-15 13:32:47 -0800

    Fix whitespace on popovers, better text

--- app/components/kitchen/Popovers.jsx
+++ app/components/kitchen/Popovers.jsx
@@ -2,33 +2,26 @@ import React from 'react';
 import View from 'reapp-ui/views/View';
 import PopoverLink from 'reapp-ui/components/PopoverLink';
 import ShowPopover from 'reapp-ui/actions/ShowPopover';
-import { Container } from 'reapp-ui/components/Grid';
 import Button from 'reapp-ui/components/Button';
 import BackButton from 'components/shared/BackButton';
+import Title from 'reapp-ui/components/Title';
 import { Link } from 'react-router';
 
-var PopoversPage = React.createClass({
+var PopoversPageContent = React.createClass({
   render() {
     return (
       <div>
-        <Container>
-          <p>
-            Popovers
-            are <PopoverLink content={this.props.content}>menus</PopoverLink>
-            that will float above an element that triggers them. Apple recommends
-            to use popovers on iPad, not smaller mobile devices. For iPhone, use
-            actions and modals.
-          </p>
-        </Container>
+        <Title>Popover Below</Title>
+        <p>
+          Popovers are <PopoverLink content={this.props.content}>menus</PopoverLink> that will float above an element that triggers them. Apple recommends to use popovers on iPad, not smaller mobile devices. For iPhone, use actions and modals.
+        </p>
 
         <br /><br /><br /><br /><br /><br /><br /><br />
 
-        <Container>
-          <p>
-            This is a popover that should trigger from the bottom of the page and point upwards.
-            <PopoverLink content={this.props.content}>Check it out</PopoverLink>.
-          </p>
-        </Container>
+        <Title>Popover Above</Title>
+        <p>
+          This is a popover that should trigger from the bottom of the page and point upwards. <PopoverLink content={this.props.content}>Check it out</PopoverLink>.
+        </p>
       </div>
     );
   }
@@ -56,7 +49,7 @@ export default React.createClass({
 
     return (
       <View {...this.props} title={[BackButton, 'Popovers', menuButton]}>
-        <PopoversPage {...this.props} content={popoverContent} />
+        <PopoversPageContent {...this.props} content={popoverContent} />
       </View>
     );
   }
