7025476e49cb2533a14e5e2ac02e1647dba7e0ef
natew <natewienert@gmail.com>
2015-02-09 20:05:54 -0800

    onTap back;

--- app/components/shared/BackButton.jsx
+++ app/components/shared/BackButton.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import BackButton from 'reapp-ui/components/buttons/BackButton';
 
 export default (
-  <BackButton onTap={function() {
-    // todo: get this to work with the staticcontainers
-    // this.props.viewListScrollToStep(0);
-
-    // todo: Change Back to Kitchen Sink, get UI to properly align them
-    window.history.back();
-  }}>
+  <BackButton onTap={window.history.back} stopPropagation>
     Back
   </BackButton>
 );
