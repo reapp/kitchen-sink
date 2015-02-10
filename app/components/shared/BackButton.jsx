ca51a75e27ec6653ab4998856e3eb7b57835f6e1
natew <natewienert@gmail.com>
2015-02-09 19:56:15 -0800

    backbutton ontap

--- app/components/shared/BackButton.jsx
+++ app/components/shared/BackButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BackButton from 'reapp-ui/components/buttons/BackButton';
 
 export default (
-  <BackButton onClick={function() {
+  <BackButton onTap={function() {
     // todo: get this to work with the staticcontainers
     // this.props.viewListScrollToStep(0);
 
