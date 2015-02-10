1f55b338db739f58ad43d6b60eb10ecdff5a23c6
natew <natewienert@gmail.com>
2015-02-09 20:07:18 -0800

    goback

--- app/components/shared/BackButton.jsx
+++ app/components/shared/BackButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BackButton from 'reapp-ui/components/buttons/BackButton';
 
 export default (
-  <BackButton onTap={window.history.back} stopPropagation>
+  <BackButton onTap={() => window.history.back()} stopPropagation>
     Back
   </BackButton>
 );
