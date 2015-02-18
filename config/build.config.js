a43731e531a4612617e5f1a2444b711a3e21d8dc
natew <natewienert@gmail.com>
2015-02-18 11:22:37 -0800

    custom builds

---
+++ config/build.config.js
@@ -0,0 +1,15 @@
+var pack = require('reapp-pack');
+var path = require('path');
+var dir = path.join(__dirname, '..');
+
+module.exports = pack({
+  dir: dir,
+  linkModules: true,
+  entry: './app/app.js',
+  devtool: 'none',
+  target: 'web',
+  debug: true,
+  env: 'production',
+  platform: 'web',
+  minify: true
+});
