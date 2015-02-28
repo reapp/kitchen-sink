15cb2716cba41b6596362c0c3d0ab839604a1d57
natew <natewienert@gmail.com>
2015-02-27 18:41:45 -0800

    moving to new structure

--- config/build.config.js
+++ config/build.config.js
@@ -1,15 +1,11 @@
-var pack = require('reapp-pack');
-var path = require('path');
-var dir = path.join(__dirname, '..');
-
-module.exports = pack({
-  dir: dir,
-  linkModules: true,
+module.exports = {
   entry: './app/app.js',
   devtool: 'none',
   target: 'web',
-  debug: true,
   env: 'production',
   platform: 'web',
-  minify: true
-});
+  linkModules: true,
+  debug: true,
+  minify: true,
+  separateStylesheets: true
+};
