9d9c3b7a4eb859b328ed23b122d1248d21e101f8
Alex Merrifield <alex.merrifield@levelupdevelopment.com>
2016-04-19 11:09:06 -0400

    restructuring kitchen sink, in same format as a new reapp new project.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,29 @@
-import 'reapp-ui';
-import 'reapp-ui/lib/desktopTouch'; // demo
+import './theme';
+import { router, route } from 'reapp-kit';
 
-import Router from 'reapp-routes/react-router';
-import Routes from './routes';
+router(require,
+  route('app', '/', { dir: '' },
+    route('kitchen', '/',
+      route('modals'),
+      route('popovers'),
+      route('bars'),
+      route('buttons'),
+      route('cards'),
+      route('drawers'),
+      route('lists'),
+      route('grids'),
+      route('forms'),
+      route('gallery'),
 
-Router(Routes);
+      // views
+      route('view-lists'),
+      route('dotted-view-lists'),
+
+      // demos
+      route('mailbox'),
+      route('twitter',
+        route('tweetView')
+      )
+    )
+  )
+);
